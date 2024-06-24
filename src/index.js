08653dfb4a414c9ab9498edbb3ad37ef5b0703bd
Jennifer Tornu <jennyslim@gmail.com>
2024-06-24 06:26:00 -0700

    adding all files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// alert('Hi there my second Work On Java Script');
+// prompt('What Is Your Name');
+alert ('Hi, Welcome To Our Websites');
+
+
+alert('Hi, Miss')
+let title= prompt('What is your Name');
+alert(title )
+alert('Hi, Jenny')
+let title= prompt('What Your Favorite electrical applaiances');
+alert(title)
